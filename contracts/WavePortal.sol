aa5a4b9c609ac974c524e839e40822ec415bf5c6
gnothyself <seanpatrick0x@gmail.com>
2021-12-13 15:42:25 -0800

    updated for random ETH winner, 23 hour cool-off period

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,22 +8,93 @@ contract WavePortal {
     uint256 totalWaves;
     uint totalHighFives;
 
-    constructor() {
-        console.log("Hello everyone and welcome to another episode of The Daily Gwei Refuel");
+    uint256 private seed;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address actor;
+        string message;
+        uint256 timestamp;
     }
 
-    function wave() public {
+    Wave[] actions;
+
+    mapping(address => uint256) public lastWavedAt;
+    
+    constructor() payable {
+        console.log("We have been constructed!");
+
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 23 hours < block.timestamp,
+            "Wait 1 day"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+        
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
+        actions.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %d", seed);
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
     }
 
-    function highFive() public {
+    function highFive(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 23 hours < block.timestamp,
+            "Wait 1 day"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalHighFives += 1;
         console.log("%s has high fived", msg.sender);
+
+        actions.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %d", seed);
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+    }
+
+    function getAllActions() public view returns (Wave[] memory) {
+        return actions;
     }
 
     function getActionTotal() public view returns (uint256) {
-        console.log("We have %d total waves and %d total high fives!", totalWaves, totalHighFives);
         return totalWaves;
     }
 }
